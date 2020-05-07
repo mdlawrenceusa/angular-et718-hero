7eb6d4cefc8214ba50f85cf04442375fa74504b0
mdlawrenceusa <mdlawrence@me.com>
2020-05-07 03:42:52 -0400

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+
+
 import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
 
-import { MdMenuModule, MdButtonModule, MdIconModule, MdCardModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';

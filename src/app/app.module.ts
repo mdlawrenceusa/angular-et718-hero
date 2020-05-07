c3194161436cd4ad231a40c76a88f0860eeed748
mdlawrenceusa <mdlawrence@me.com>
2020-05-07 03:34:58 -0400

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
 
+import { MdMenuModule, MdButtonModule, MdIconModule, MdCardModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeroComponent } from './hero.component';
